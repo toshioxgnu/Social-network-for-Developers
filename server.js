9e074a0959e51b5a334328e77f897c9028763169
Jose Gonzalez <jgonzalezr5@outlook.es>
2020-06-02 20:22:17 -0400

    Se elimina prueba

--- server.js
+++ server.js
@@ -7,5 +7,3 @@ app.get('/', (req,res) => res.send('API Running'));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log('Sever started on port ${PORT} '));
-
-testeando cosas
